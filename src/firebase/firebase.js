9979b29a2e63237dd9de737c2294f5cf377fd156
Bilash Ghosh <156583497+bilashghosh@users.noreply.github.com>
2024-12-08 17:46:32 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,15 +4,7 @@ import {getAuth} from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD3VTahW0hlZ-EaR2P9aC6wpk8f2Ybjse8",
-  authDomain: "academy-of-exam.firebaseapp.com",
-  projectId: "academy-of-exam",
-  storageBucket: "academy-of-exam.appspot.com",
-  messagingSenderId: "714674313240",
-  appId: "1:714674313240:web:0dee48e8e5ed6e23830b6d",
-  measurementId: "G-VBT6C0NJTG"
-};
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
